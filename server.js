6640a33a4d7407fd6fc8222facb0be3248de3e9e
Ming Huo <mhuo0searshc.com>
2017-05-12 20:22:28 -0700

    add server js file

---
+++ server.js
@@ -0,0 +1,13 @@
+//using express with node js
+var express = require('express');
+
+//initialize app as an express application
+var app = express();
+
+var ipaddress = '127.0.0.1';
+var port      = 3000;
+
+app.use(express.static(__dirname+'/public'));
+app.listen(port, ipaddress);
+
+console.log("hello world1");
