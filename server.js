eec9e8ec6426ddbeb1190612b79023abea668a5d
ming.huo <ming.huo@gmail.com>
2017-05-20 16:03:00 -0700

    change port number

--- server.js
+++ server.js
@@ -4,12 +4,9 @@ var express = require('express');
 //initialize app as an express application
 var app = express();
 
-//var ipaddress = '127.0.0.1';
-var port      = process.env.port;
-
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname+'/public'));
-//app.listen(port, ipaddress);
-app.listen(port);
 
-console.log("hello world!");
-console.log(process.env.port)
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
