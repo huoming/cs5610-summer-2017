1ffc605075334e1a2bb0410cf310cdbaf3ad2a7d
ming.huo <ming.huo@gmail.com>
2017-05-20 15:39:45 -0700

    remove ipaddress in server.js file

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ var express = require('express');
 //initialize app as an express application
 var app = express();
 
-var ipaddress = '127.0.0.1';
+//var ipaddress = '127.0.0.1';
 var port      = process.env.PORT || 3000;
 
 app.use(express.static(__dirname+'/public'));
-app.listen(port, ipaddress);
+//app.listen(port, ipaddress);
+app.listen(port);
 
 console.log("hello world!");
