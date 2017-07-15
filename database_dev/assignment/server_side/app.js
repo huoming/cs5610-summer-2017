6f37746525f35cef2e19336833784c786e144c07
ming.huo <ming.huo@gmail.com>
2017-07-14 18:16:47 -0700

    add some sample simple sort examples. it may not work properly.

--- database_dev/assignment/server_side/app.js
+++ database_dev/assignment/server_side/app.js
@@ -1,5 +1,6 @@
 module.exports = function(app){
     var models = require("./model/models.server.js")();
+
     require("./services/user.service.server.js")(app, models);
     require("./services/website.service.server.js")(app, models);
     require("./services/page.service.server")(app, models);
